07fdeb0fe45fc6c8be068e3eaa9074166cd884fe
ThaissaLeslye <thaissa.leslye.tl@gmail.com>
2025-05-11 17:23:32 -0300

    Adding sections valores to a-liga.html (incomplete)

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,15 @@ async function loadComponent(id, file) {
 loadComponent("header", "header.html");
 loadComponent("a-liga", "a-liga.html");
 loadComponent("footer", "footer.html");
+
+//Slide - A Liga - Valores 
+let showingFirst = true;
+
+function toggleText() {
+  document.getElementById('text1').classList.toggle('active');
+  document.getElementById('text2').classList.toggle('active');
+  document.getElementById('elipse1').classList.toggle('active');
+  document.getElementById('elipse2').classList.toggle('active');
+  showingFirst = !showingFirst;
+}
+	
