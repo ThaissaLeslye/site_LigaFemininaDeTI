e3da7a7b6288e96de472bea96109f88c3a671aa1
ThaissaLeslye <thaissa.leslye.tl@gmail.com>
2025-05-11 21:50:28 -0300

    Adding section destaques to a-liga.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,22 @@
 async function loadComponent(id, file) {
-  const res = await fetch(file);
-  const content = await res.text();
-  document.getElementById(id).innerHTML = content;
+	  const res = await fetch(file);
+	  const content = await res.text();
+	  document.getElementById(id).innerHTML = content;
 }
 
 loadComponent("header", "header.html");
 loadComponent("a-liga", "a-liga.html");
 loadComponent("footer", "footer.html");
 
-//Slide - A Liga - Valores 
+//Slide - A Liga - Valores
 let showingFirst = true;
 
-function toggleText() {
-  document.getElementById('text1').classList.toggle('active');
-  document.getElementById('text2').classList.toggle('active');
-  document.getElementById('elipse1').classList.toggle('active');
-  document.getElementById('elipse2').classList.toggle('active');
-  showingFirst = !showingFirst;
+function changeSlide() {
+	  document.getElementById("text1").classList.toggle("active");
+	  document.getElementById("text2").classList.toggle("active");
+    document.getElementById("elipse1").classList.toggle("active");
+    document.getElementById("elipse2").classList.toggle("active");
+	  document.getElementById("next-btn1").classList.toggle("active");
+    document.getElementById("next-btn2").classList.toggle("active");
+	  showingFirst = !showingFirst;
 }
-	
