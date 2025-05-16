35bf85efd2b9e1c0b8fcb476413f5c56806c4fcd
ThaissaLeslye <thaissa.leslye.tl@gmail.com>
2025-05-16 14:31:46 -0300

    Lot of changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,41 @@
 // Insert html files into index.html
-async function loadComponent(className, file) {
+async function loadComponent(className, file, callback) {
 	const res = await fetch(file);
 	const content = await res.text();
 	const element = document.querySelector(`.${className}`);
 
 	if (element) {
 		element.innerHTML = content
+		window.scrollTo(0, 0);
+		
+		if (typeof callback === 'function') {
+			callback();
+		}
 	}
 }
 
 // Changes main's content calling loadComponent
-function changeMain(newClass, htmlFile) {
+function changeMain(newClass, htmlFile, targetSectionId = null) {
   	const mainElement = document.getElementById("main");
 	if (!mainElement) return;
 
 	mainElement.className = newClass;
 
-	loadComponent(newClass, htmlFile);
+	const afterLoadCallback = () => {
+		if (targetSectionId) {
+			const targetElement = document.getElementById(targetSectionId);
+			if (targetElement) {
+				targetElement.scrollIntoView();
+			}
+		}
+	};
+
+	if (newClass === 'integrantes') {
+		loadComponent(newClass, htmlFile, renderMembersPanel);
+	} else {
+		loadComponent(newClass, htmlFile, afterLoadCallback);
+	}
+
 
 	localStorage.setItem("currentClass", newClass);
 	localStorage.setItem("currentFile", htmlFile);
@@ -35,11 +54,40 @@ function loadLastMain() {
 	loadComponent(savedClass, savedFile);
 }
 
-loadLastMain();
-
 // Slides
 function changeSlide(className) {
 	document.querySelectorAll(`.${className}`).forEach(element => {
 		element.classList.toggle("active");
 	});
 }
+
+// Integrantes panel items injection
+function renderMembersPanel() {
+	fetch('/assets/data/membros.json')
+		.then(response => response.json())
+		.then(data => {
+			const painel = document.getElementById('painel');
+		
+			data.membros.forEach(integrante => {
+			const item = document.createElement('div');
+			item.classList.add('item');
+
+			item.innerHTML = `
+				<div class="imagem-wrapper">
+					<img src="${integrante.foto}" alt="${integrante.nome}">
+					<div class="overlay">
+					<a href="${integrante.linkedin}" target="_blank">LinkedIn</a>
+					</div>
+				</div>
+				<h3>${integrante.nome}</h3>
+				<span>${integrante.cargo}</span>
+			`;
+
+			painel.appendChild(item);
+			});
+		})
+		.catch(error => console.error('Erro ao carregar os dados:', error));
+}
+
+loadLastMain();
+renderMembersPanel();
